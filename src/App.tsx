f139f6aeadcc12933f4bba82811094e28dad50f8
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-10-21 10:08:38 -0300

    app updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import { ProductsPage } from "./pages/ProductPage";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+  return <ProductsPage />;
 }
 
 export default App;
