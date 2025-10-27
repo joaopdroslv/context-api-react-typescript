5ae4cd779f7aa5a557a36be647b1efc538bbb0f9
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-10-27 14:46:07 -0300

    styling changes

--- src/components/Product/ProductsList.tsx
+++ src/components/Product/ProductsList.tsx
@@ -11,32 +11,32 @@ export const ProductsList: React.FC = () => {
     <table className="min-w-full shadow-md rounded-md overflow-hidden divide-y divide-gray-200">
       <thead className="bg-blue-600 text-white">
         <tr>
-          <th className="px-3 py-3 text-left text-sm font-medium uppercase">
+          <th className="px-3 py-3 text-left text-sm font-medium">
             #
           </th>
-          <th className="px-3 py-3 text-left text-sm font-medium uppercase">
+          <th className="px-3 py-3 text-left text-sm font-medium">
             Name
           </th>
-          <th className="px-3 py-3 text-left text-sm font-medium uppercase">
+          <th className="px-3 py-3 text-left text-sm font-medium">
             Category
           </th>
-          <th className="px-3 py-3 text-left text-sm font-medium uppercase">
+          <th className="px-3 py-3 text-left text-sm font-medium">
             Price
           </th>
-          <th className="px-3 py-3 text-left text-sm font-medium uppercase">
+          <th className="px-3 py-3 text-left text-sm font-medium">
             Rate
           </th>
-          <th className="px-3 py-3 text-left text-sm font-medium uppercase">
-            Rates
+          <th className="px-3 py-3 text-left text-sm font-medium">
+            Rate Count
           </th>
         </tr>
       </thead>
       <tbody className="divide-y divide-gray-200">
         {products.map((product: Product) => (
-          <tr className="hover:bg-gray-100">
+          <tr className="hover:bg-gray-100 cursor-pointer">
             <td className="px-3 py-3 text-sm">{product.id}</td>
             <td className="px-3 py-3 text-sm">{product.title}</td>
-            <td className="px-3 py-3 text-sm">{product.category}</td>
+            <td className="px-3 py-3 text-sm capitalize">{product.category}</td>
             <td className="px-3 py-3 text-sm">$ {product.price}</td>
             <td className="px-3 py-3 text-sm">{product.rating.rate}</td>
             <td className="px-3 py-3 text-sm">{product.rating.count}</td>
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -8,10 +8,10 @@ export const ProductsPage: React.FC = () => {
     <ProductsProvider>
       <div className="w-full h-full bg-gray-100">
         <main className="h-full w-4/5 mx-auto flex flex-col items-center py-6">
-          <section className="w-full bg-white py-10 px-10 rounded-md mb-4 flex items-center justify-center">
+          <section className="w-full bg-white py-8 px-8 rounded-md mb-4 flex items-center justify-center">
             <ProductsFilter />
           </section>
-          <section className="w-full bg-white py-10 px-10 rounded-md mb-4 flex items-center justify-center">
+          <section className="w-full bg-white py-8 px-8 rounded-md mb-4 flex items-center justify-center">
             <ProductsList />
           </section>
         </main>
