951f910a29880946b26f00b93f52556dce1cf8cc
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-10-27 12:09:59 -0300

    products filter updated

--- src/components/Product/ProductsFilter.tsx
+++ src/components/Product/ProductsFilter.tsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useProducts } from "../../hooks/useProducts";
+import { TagsInput } from "../ui/TagsInput";
+import { MultiSelectInput } from "../ui/MultiSelectInput";
+import { categories as availableCategories } from "../../database";
 
 export const ProductsFilter: React.FC = () => {
-  const { setFilters } = useProducts();
+  const [keywords, setKeywords] = useState<string[]>([]);
+  const [categories, setCategories] = useState<number[]>([]);
+  const { updateFilters } = useProducts();
 
-  return <div>component to filter products</div>;
+  useEffect(() => {
+    updateFilters("keywords", keywords);
+    updateFilters("categoriesIds", categories);
+  }, [keywords, categories]);
+
+  return (
+    <div className="w-full grid grid-cols-6 gap-4">
+      <div className="col-span-2">
+        <label htmlFor="keywords" className="block mb-2 text-sm font-medium">
+          By name
+        </label>
+        <TagsInput
+          tags={keywords}
+          setTags={setKeywords}
+          placeholder="Use one/many keywords..."
+        />
+      </div>
+      <div className="col-span-2">
+        <label htmlFor="categories" className="block mb-2 text-sm font-medium">
+          By category
+        </label>
+        <MultiSelectInput
+          options={availableCategories}
+          selected={categories}
+          setSelected={setCategories}
+          keyToSelect="id"
+          keyToDisplay="name"
+          placeholder="Select one/many categories..."
+        />
+      </div>
+    </div>
+  );
 };
