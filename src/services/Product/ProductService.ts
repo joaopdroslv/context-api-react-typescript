6edb42dd91ffee2adbcb32ab907b5170e8126fb3
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-10-30 13:16:29 -0300

    sleep util created

--- src/services/Product/ProductService.ts
+++ src/services/Product/ProductService.ts
@@ -1,7 +1,9 @@
 import { BaseService } from "../BaseService";
 import type { Product } from "../../models/Product/Product";
-import { products, productFiltersParams } from "../../database";
+import { products, productFiltersParams, biData } from "../../database";
 import type { ProductFiltersParams } from "../../models/Product/ProductFiltersParams";
+import type { BiData } from "../../models/BusinessIntelligence/BiData";
+import { sleep } from "../../utils/Utils";
 
 const baseUrl = "BASE_URL_HERE";
 
@@ -11,13 +13,16 @@ export class ProductService extends BaseService<Product> {
   }
 
   async getAll(): Promise<Product[]> {
-    const sleep = (ms: number) =>
-      new Promise((resolve) => setTimeout(resolve, ms));
     await sleep(5000);
     return products;
   }
 
   async getParams(): Promise<ProductFiltersParams> {
     return productFiltersParams;
+  }
+
+  async getBiData(): Promise<BiData> {
+    await sleep(10000)
+    return biData;
   }
 }
